c5129d6a5ede7a522adcaf003cb86899a2f679a2
Hp <Hp@KRIS>
2023-09-23 21:01:00 +0300

    Добавьте файлы проекта.

---
+++ task 1/task 1.cpp
@@ -0,0 +1,60 @@
+﻿/*при выполнении данной задачи вы должны сравнить библиотеки “string.h” или <cstring>(с) и
+<string>(с++).
+а) перед main() в комментариях ответьте на вопросы и напишите соответствия с – с++ для
+следующих функций(причем соответствия необязательно будут в виде функций) :
+	1) strlen - string::length
+	2) strcpy - аналога нет, поэтому пишем (a = b)
+	3) strcmp - аналога нет
+	4) strcat - someString.append
+	5) somestring[index] - someString.at(index)
+	6) не имеет точного аналога (возможно использовать strcat)  - push_back
+	7) не имеет аналога - pop_back
+
+	функции могут не иметь точного аналога в библиотеке другого языка.почему ? – объясните главную
+	разницу строк в с и с++.
+
+	Строки в C - массив элементов char с null-байтом в окончании
+	Строки в C++ - обЪект класса string с динамической памятью
+
+	б) теперь напишите программу на с++, которая будет сортировать буквы в алфавитном порядке в
+	веденной пользователем строке.причем программа должна удалять пробелы и пунктуационные
+	знаки.
+	nb: используйте только библиотеку <string>.
+	*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string input;
+
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, input);
+
+    // Удаляем пробелы и пунктуационные знаки
+    std::string cleanedInput;
+    for (char c : input) {
+        if (!std::isspace(c) && !std::ispunct(c)) {
+            cleanedInput += c;
+        }
+    }
+
+    // Сортируем буквы в алфавитном порядке
+    for (int i = 0; i < cleanedInput.length() - 1; i++) {
+        for (int j = 0; j < cleanedInput.length() - i - 1; j++) {
+            if (cleanedInput[j] > cleanedInput[j + 1]) {
+                char temp = cleanedInput[j];
+                cleanedInput[j] = cleanedInput[j + 1];
+                cleanedInput[j + 1] = temp;
+            }
+        }
+    }
+
+    std::cout << "Результат: " << cleanedInput << std::endl;
+
+    return 0;
+}
+
+
+	
+
---
+++ task 2/task 2.cpp
@@ -0,0 +1,75 @@
+﻿//А) Перед main() в комментариях ответьте на вопросы и напишите соответствия С – С++ для
+//следующих операций :
+//1) int* ptr = (int*)malloc(sizeof(int)) - int* ptr = new int
+//2) int* ptrArr = (int*)malloc(sizeof(int) * N) - int* ptrArr = new int[N]
+//3) free(ptr) - delete ptr
+//4) free(ptrArr) - delete[] ptrArr
+//В С++ необходимо ли освобождать динамическую память ? 
+// Да, оператором delete.
+//Можно ли освободить одну и ту же выделенную память дважды ? 
+// Нет, это приводит к недокументированному поведению.
+//А можно ли освободить невыделенную память ? 
+// Нет.
+// 
+// 
+// 
+//Б) Треугольник Фибоначчи.У некоторых это задание было на программировании по С.Теперь
+//требуется адаптировать эту программу(или написать заново) на С++ с определенными
+//требованиями(NB) :
+//	-именно пользователь должен решать(посредством ввода в командную строку) сколько строк у
+//	пирамиды должно быть;
+//-перемещаться по массиву(-ам) необходимо только через указательную арифметику – Pointer
+//Arithmetic[arr[i][j] - > *(*(arr + i) + j)];
+//-использовать только динамическое выделение памяти
+
+#include <iostream>
+
+int** createFibonacciTriangle(int rows) {
+    int** triangle = new int* [rows];
+
+    for (int i = 0; i < rows; i++) {
+        triangle[i] = new int[i + 1];
+        triangle[i][0] = 1;
+        triangle[i][i] = 1;
+
+        for (int j = 1; j < i; j++) {
+            *(triangle[i] + j) = *(triangle[i - 1] + j - 1) + *(triangle[i - 1] + j);
+        }
+    }
+
+    return triangle;
+}
+
+void printFibonacciTriangle(int** triangle, int rows) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j <= i; j++) {
+            std::cout << *(*(triangle + i) + j) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void deleteFibonacciTriangle(int** triangle, int rows) {
+    for (int i = 0; i < rows; i++) {
+        delete[] triangle[i];
+    }
+    delete[] triangle;
+}
+
+int main() {
+    int rows;
+
+    std::cout << "Введите количество строк: ";
+    std::cin >> rows;
+
+    // Создание треугольника Фибоначчи
+    int** fibonacciTriangle = createFibonacciTriangle(rows);
+
+    // Вывод треугольника Фибоначчи
+    printFibonacciTriangle(fibonacciTriangle, rows);
+
+    // Освобождение памяти
+    deleteFibonacciTriangle(fibonacciTriangle, rows);
+
+    return 0;
+}
